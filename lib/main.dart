72f42f54f2e8f7c906c50d65a586a545007bb6e3
Sherryl <sherrylsin20@gmail.com>
2020-12-12 19:38:23 +0700

    Implemented pageView and curved navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:resapp/screens/categories.dart';
+import 'package:resapp/screens/home.dart';
+import 'package:resapp/screens/saved.dart';
 
-void main() => runApp(ResApp());
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ResApp',
+      home: ResApp(),
+    );
+  }
+}
 
 class ResApp extends StatefulWidget {
   @override
@@ -9,39 +22,80 @@ class ResApp extends StatefulWidget {
 }
 
 class _ResAppState extends State<ResApp> {
+  int _page = 1;
+
+  final PageController controller =
+      PageController(initialPage: 1, keepPage: true);
+
+  GlobalKey _bottomNavigationKey = GlobalKey();
+
+  Widget buildPageView() {
+    return PageView(
+      controller: controller,
+      onPageChanged: (index) {
+        pageChanged(index);
+      },
+      children: <Widget>[CategoriesPage(), HomePage(), SavedPage()],
+    );
+  }
+
   @override
+  void initState() {
+    super.initState();
+  }
+
+  void pageChanged(int index) {
+    setState(() {
+      _page = index;
+    });
+  }
+
+  void bottomTapped(int index) {
+    setState(() {
+      _page = index;
+      controller.animateToPage(index,
+          duration: Duration(milliseconds: 500), curve: Curves.linear);
+    });
+  }
+
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white12,
         title: Image.asset('assets/images/resapp.png', width: 100),
         centerTitle: true,
         elevation: 0,
       ),
+      body: buildPageView(),
       bottomNavigationBar: CurvedNavigationBar(
+          key: _bottomNavigationKey,
           backgroundColor: Colors.white12,
           color: Colors.deepOrange,
+          index: _page,
+          animationDuration: Duration(milliseconds: 500),
           items: <Widget>[
             Icon(
               Icons.list,
-              size: 30,
+              size: 35,
               color: Colors.white,
             ),
             Icon(
               Icons.home,
-              size: 30,
+              size: 35,
               color: Colors.white,
             ),
             Icon(
               Icons.bookmark,
-              size: 30,
+              size: 35,
               color: Colors.white,
             )
           ],
           onTap: (index) {
-            //Handle button tap
+            debugPrint(index.toString());
+            setState(() {
+              bottomTapped(index);
+            });
           }),
-    ));
+    );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SavedPage extends StatelessWidget {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-      child: Text('Saved'),
+      child: Text('Homepage'),
     ));
   }
 }
